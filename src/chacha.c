f380a3b4715eb792ca839ee97e413abe50158962
cheems <yangry2023@shanghaitech.edu.cn>
2025-05-20 16:29:14 +0800

    feat: chacha simd 256

--- src/chacha.c
+++ src/chacha.c
@@ -1,154 +1,119 @@
 #include "mercha.h"
 #include <immintrin.h>
 
-// Helper for ROTL32 on __m128i 4int
-#define rotl32_128(x, n)                                                       \
-  _mm_or_si128(_mm_slli_epi32((x), (n)), _mm_srli_epi32((x), 32 - (n)))
+// Helper for ROTL32 on __m256i
+#define rotl32_256(x, n)                                                       \
+  _mm256_or_si256(_mm256_slli_epi32((x), (n)), _mm256_srli_epi32((x), 32 - (n)))
 
-#define chacha_simd(a, b, c, d)                                                \
-  (a) = _mm_add_epi32((a), (b));                                               \
-  (d) = _mm_xor_si128((d), (a));                                               \
-  (d) = rotl32_128((d), 16);                                                   \
-  (c) = _mm_add_epi32((c), (d));                                               \
-  (b) = _mm_xor_si128((b), (c));                                               \
-  (b) = rotl32_128((b), 12);                                                   \
-  (a) = _mm_add_epi32((a), (b));                                               \
-  (d) = _mm_xor_si128((d), (a));                                               \
-  (d) = rotl32_128((d), 8);                                                    \
-  (c) = _mm_add_epi32((c), (d));                                               \
-  (b) = _mm_xor_si128((b), (c));                                               \
-  (b) = rotl32_128((b), 7)
+#define chacha_simd_avx2(a, b, c, d)                                           \
+  (a) = _mm256_add_epi32((a), (b));                                            \
+  (d) = _mm256_xor_si256((d), (a));                                            \
+  (d) = rotl32_256((d), 16);                                                   \
+  (c) = _mm256_add_epi32((c), (d));                                            \
+  (b) = _mm256_xor_si256((b), (c));                                            \
+  (b) = rotl32_256((b), 12);                                                   \
+  (a) = _mm256_add_epi32((a), (b));                                            \
+  (d) = _mm256_xor_si256((d), (a));                                            \
+  (d) = rotl32_256((d), 8);                                                    \
+  (c) = _mm256_add_epi32((c), (d));                                            \
+  (b) = _mm256_xor_si256((b), (c));                                            \
+  (b) = rotl32_256((b), 7)
 
-static void chacha20_block(uint32_t state[16], uint8_t out[64]) {
+static void chacha20_double_block_avx2(const uint32_t state_template[16],
+                                       uint32_t counter_block0,
+                                       uint8_t out[128]) {
 
-  __m128i v_s0_3, v_s4_7, v_s8_11, v_s12_15;
-  __m128i v_x0_3, v_x4_7, v_x8_11, v_x12_15;
+  __m256i v_s0_3, v_s4_7, v_s8_11, v_s12_15;
+  __m256i v_x0_3, v_x4_7, v_x8_11, v_x12_15;
 
-  // State
-  v_s0_3 = _mm_loadu_si128((const __m128i *)&state[0]);
-  v_s4_7 = _mm_loadu_si128((const __m128i *)&state[4]);
-  v_s8_11 = _mm_loadu_si128((const __m128i *)&state[8]);
-  v_s12_15 = _mm_loadu_si128((const __m128i *)&state[12]);
+  uint32_t current_state1[16], current_state2[16];
 
-  // X
+  memcpy(current_state1, state_template, 16 * sizeof(uint32_t));
+  memcpy(current_state2, state_template, 16 * sizeof(uint32_t));
+
+  current_state1[12] = counter_block0;
+  current_state2[12] = counter_block0 + 1;
+
+  // Load states: low 128-bits for current_state1, high 128-bits for
+  // current_state2
+  v_s0_3 =
+      _mm256_set_m128i(_mm_loadu_si128((const __m128i *)&current_state2[0]),
+                       _mm_loadu_si128((const __m128i *)&current_state1[0]));
+  v_s4_7 =
+      _mm256_set_m128i(_mm_loadu_si128((const __m128i *)&current_state2[4]),
+                       _mm_loadu_si128((const __m128i *)&current_state1[4]));
+  v_s8_11 =
+      _mm256_set_m128i(_mm_loadu_si128((const __m128i *)&current_state2[8]),
+                       _mm_loadu_si128((const __m128i *)&current_state1[8]));
+  v_s12_15 =
+      _mm256_set_m128i(_mm_loadu_si128((const __m128i *)&current_state2[12]),
+                       _mm_loadu_si128((const __m128i *)&current_state1[12]));
+
   v_x0_3 = v_s0_3;
   v_x4_7 = v_s4_7;
   v_x8_11 = v_s8_11;
   v_x12_15 = v_s12_15;
 
-  // Rounds
+  // 10 double rounds (20 quarter rounds)
+  for (int i = 0; i < 10; ++i) {
+    // Column rounds
+    chacha_simd_avx2(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
+    // Diagonal rounds
+    // Shuffle (rotate) elements within each 128-bit lane
+    v_x4_7 = _mm256_shuffle_epi32(v_x4_7,
+                                  _MM_SHUFFLE(0, 3, 2, 1)); // Rotate left by 1
+    v_x8_11 = _mm256_shuffle_epi32(v_x8_11,
+                                   _MM_SHUFFLE(1, 0, 3, 2)); // Rotate left by 2
+    v_x12_15 = _mm256_shuffle_epi32(
+        v_x12_15, _MM_SHUFFLE(2, 1, 0, 3)); // Rotate left by 3
 
-  // Round 1
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
+    chacha_simd_avx2(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
 
-  // Round 2
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 3
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 4
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 5
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 6
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 7
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 8
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 9
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
-
-  // Round 10
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(0, 3, 2, 1));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(2, 1, 0, 3));
-  chacha_simd(v_x0_3, v_x4_7, v_x8_11, v_x12_15);
-  v_x4_7 = _mm_shuffle_epi32(v_x4_7, _MM_SHUFFLE(2, 1, 0, 3));
-  v_x8_11 = _mm_shuffle_epi32(v_x8_11, _MM_SHUFFLE(1, 0, 3, 2));
-  v_x12_15 = _mm_shuffle_epi32(v_x12_15, _MM_SHUFFLE(0, 3, 2, 1));
+    // Shuffle back (rotate right) elements within each 128-bit lane
+    v_x4_7 = _mm256_shuffle_epi32(
+        v_x4_7, _MM_SHUFFLE(2, 1, 0, 3)); // Rotate right by 1 (left by 3)
+    v_x8_11 = _mm256_shuffle_epi32(
+        v_x8_11, _MM_SHUFFLE(1, 0, 3, 2)); // Rotate right by 2 (left by 2)
+    v_x12_15 = _mm256_shuffle_epi32(
+        v_x12_15, _MM_SHUFFLE(0, 3, 2, 1)); // Rotate right by 3 (left by 1)
+  }
 
   // Add initial state
-  v_x0_3 = _mm_add_epi32(v_x0_3, v_s0_3);
-  v_x4_7 = _mm_add_epi32(v_x4_7, v_s4_7);
-  v_x8_11 = _mm_add_epi32(v_x8_11, v_s8_11);
-  v_x12_15 = _mm_add_epi32(v_x12_15, v_s12_15);
+  v_x0_3 = _mm256_add_epi32(v_x0_3, v_s0_3);
+  v_x4_7 = _mm256_add_epi32(v_x4_7, v_s4_7);
+  v_x8_11 = _mm256_add_epi32(v_x8_11, v_s8_11);
+  v_x12_15 = _mm256_add_epi32(v_x12_15, v_s12_15);
 
-  // Store results
-  _mm_storeu_si128((__m128i *)(out + 0), v_x0_3);
-  _mm_storeu_si128((__m128i *)(out + 16), v_x4_7);
-  _mm_storeu_si128((__m128i *)(out + 32), v_x8_11);
-  _mm_storeu_si128((__m128i *)(out + 48), v_x12_15);
+  // Store results: block0 keystream, then block1 keystream
+  _mm_storeu_si128((__m128i *)(out + 0), _mm256_castsi256_si128(v_x0_3));
+  _mm_storeu_si128((__m128i *)(out + 64), _mm256_extracti128_si256(v_x0_3, 1));
+
+  _mm_storeu_si128((__m128i *)(out + 16), _mm256_castsi256_si128(v_x4_7));
+  _mm_storeu_si128((__m128i *)(out + 64 + 16),
+                   _mm256_extracti128_si256(v_x4_7, 1));
+
+  _mm_storeu_si128((__m128i *)(out + 32), _mm256_castsi256_si128(v_x8_11));
+  _mm_storeu_si128((__m128i *)(out + 64 + 32),
+                   _mm256_extracti128_si256(v_x8_11, 1));
+
+  _mm_storeu_si128((__m128i *)(out + 48), _mm256_castsi256_si128(v_x12_15));
+  _mm_storeu_si128((__m128i *)(out + 64 + 48),
+                   _mm256_extracti128_si256(v_x12_15, 1));
+}
+
+// Helper function to XOR buffer with keystream
+static inline void xor_buffer_simd(uint8_t *buf, const uint8_t *key_stream,
+                                   size_t len) {
+  size_t i = 0;
+  for (; i + 15 < len; i += 16) {
+    __m128i b_chunk = _mm_loadu_si128((const __m128i *)(buf + i));
+    __m128i k_chunk = _mm_loadu_si128((const __m128i *)(key_stream + i));
+    b_chunk = _mm_xor_si128(b_chunk, k_chunk);
+    _mm_storeu_si128((__m128i *)(buf + i), b_chunk);
+  }
+  for (; i < len; ++i) {
+    buf[i] ^= key_stream[i];
+  }
 }
 
 void chacha20_encrypt(const uint8_t key[32], const uint8_t nonce[12],
@@ -161,37 +126,56 @@ void chacha20_encrypt(const uint8_t key[32], const uint8_t nonce[12],
   memcpy(key_words, key, 32);
   memcpy(nonce_words, nonce, 12);
 
-  uint32_t state[16] = {
-      0x61707865,      0x3320646e,     0x79622d32,     0x6b206574,
-      key_words[0],    key_words[1],   key_words[2],   key_words[3],
-      key_words[4],    key_words[5],   key_words[6],   key_words[7],
-      initial_counter, nonce_words[0], nonce_words[1], nonce_words[2]};
+  // This is the template state. The counter part (state[12]) will be set per
+  // block.
+  const uint32_t state_template[16] = {0x61707865,
+                                       0x3320646e,
+                                       0x79622d32,
+                                       0x6b206574,
+                                       key_words[0],
+                                       key_words[1],
+                                       key_words[2],
+                                       key_words[3],
+                                       key_words[4],
+                                       key_words[5],
+                                       key_words[6],
+                                       key_words[7],
+                                       0, // Counter placeholder
+                                       nonce_words[0],
+                                       nonce_words[1],
+                                       nonce_words[2]};
 
-  size_t num_blocks = (length + 63) / 64;
+  size_t num_total_blocks = (length + 63) / 64;
 
 #pragma omp parallel for
-  for (size_t block_idx = 0; block_idx < num_blocks; ++block_idx) {
-    uint32_t local_state[16];
-    memcpy(local_state, state, sizeof(state));
-    local_state[12] = initial_counter + (uint32_t)block_idx;
+  for (size_t block_idx_base = 0; block_idx_base < num_total_blocks;
+       block_idx_base += 2) {
+    uint8_t key_stream_pair[128]; // For two blocks
+    uint32_t counter_for_block0 = initial_counter + (uint32_t)block_idx_base;
 
-    uint8_t key_stream[64];
-    chacha20_block(local_state, key_stream);
+    chacha20_double_block_avx2(state_template, counter_for_block0,
+                               key_stream_pair);
 
-    size_t current_block_offset = block_idx * 64;
-    size_t remaining_length = length - current_block_offset;
-    size_t bytes_in_block = (remaining_length < 64) ? remaining_length : 64;
-
-    uint8_t *buf_ptr = buffer + current_block_offset;
-    size_t i = 0;
-    for (; i + 15 < bytes_in_block; i += 16) {
-      __m128i b_chunk = _mm_loadu_si128((const __m128i *)(buf_ptr + i));
-      __m128i k_chunk = _mm_loadu_si128((const __m128i *)(key_stream + i));
-      b_chunk = _mm_xor_si128(b_chunk, k_chunk);
-      _mm_storeu_si128((__m128i *)(buf_ptr + i), b_chunk);
+    // XOR for the first block in the pair (block_idx_base)
+    size_t offset0 = block_idx_base * 64;
+    if (offset0 < length) { // Check if this block is part of the message length
+      size_t remaining_length0 = length - offset0;
+      size_t bytes_in_block0 =
+          (remaining_length0 < 64) ? remaining_length0 : 64;
+      xor_buffer_simd(buffer + offset0, key_stream_pair, bytes_in_block0);
     }
-    for (; i < bytes_in_block; ++i) {
-      buf_ptr[i] ^= key_stream[i];
+
+    // XOR for the second block in the pair (block_idx_base + 1), if it exists
+    if (block_idx_base + 1 < num_total_blocks) {
+      size_t offset1 = (block_idx_base + 1) * 64;
+      if (offset1 <
+          length) { // Check if this block is part of the message length
+        size_t remaining_length1 = length - offset1;
+        size_t bytes_in_block1 =
+            (remaining_length1 < 64) ? remaining_length1 : 64;
+        xor_buffer_simd(buffer + offset1, key_stream_pair + 64,
+                        bytes_in_block1);
+      }
     }
   }
 }
